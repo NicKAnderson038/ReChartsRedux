1998a43b9c412889feceb19f58537619652fb496
Nick Anderson <andersonnick038@gmail.com>
2019-09-02 20:09:38 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { store } from './store/store.config'
+import { store } from './store/store.config.js'
 import "./styles.css";
 
 import MainRouter from './router'
 
-function App() {
-  return (
-    <div className="App">
-      <Provider store={store}>
-        <MainRouter />
-      </Provider>
-    </div>
-  )
-}
+// function App() {
+//   return (
+//     // <div className="App">
+//       <Provider store={store}>
+//         <Nav />
+//         <MainRouter />
+//       </Provider>
+//     // </div>
+//   )
+// }
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(<App />, rootElement)
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(<App />, rootElement)
 
-// const app = (
-//   <Provider store={store}>
-//   <MainRouter />
-//  </Provider>
-// );
+const app = (
+  <Provider store={store}>
+    <MainRouter />
+ </Provider>
+);
 
-// ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'));
